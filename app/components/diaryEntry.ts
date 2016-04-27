b7de61e7c694057c1fd501bd0e25bc18346d0815
ayush <ayush15may@gmail.com>
2016-04-28 00:03:38 +0530

    work in progress on event binding and timing

--- app/components/diaryEntry.ts
+++ app/components/diaryEntry.ts
@@ -6,11 +6,29 @@ import {LoginForm} from './loginForm';
     template: `
     <div class="diary-entry-box">
     <form id="diaryEntryForm">
-		<textarea id= "diary-entry-textarea" form="diaryEntryForm" placeholder= "hi ..." rows=8></textarea>
+		<textarea id= "diary-entry-textarea" #textArea (focus)="startTimer(textArea)" (keyup)= "resetTimer(textArea)" form="diaryEntryForm" placeholder= "hi ..." rows=8></textarea>
         <br/>
         <input id="diary-entry-submit" type = submit value="That's all">
         </form>
 	</div>
     `
 })
-export class DiaryEntry { }
+export class DiaryEntry {
+
+    myTimer: any;
+
+    startTimer(st: HTMLInputElement) {
+        this.myTimer = setInterval(this.myTimerFunction(st), 2000);        
+    }   
+myTimerFunction(st: HTMLInputElement) {
+        st.value = null;
+        console.log("clear textarea")
+    }
+    
+    resetTimer(rt: HTMLInputElement) {
+        // this.stuff.push(st.value);
+        console.log("clear Timer")
+        clearInterval(this.myTimer);
+        this.myTimer = setInterval(this.myTimerFunction(rt), 2000); 
+    }
+}
